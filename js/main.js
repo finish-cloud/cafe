ebed7ba617747b83bb52773c6618e5b8c1b869e6
masa_81 <hope.is.a.waking.dream.8.1@gmail.com>
2022-09-30 00:08:07 +0900

    ハンバーガーメニューを追加

--- js/main.js
+++ js/main.js
@@ -9,3 +9,36 @@ $(function () {
     return false;
   });
 })
+
+/*ハンバーガー*/
+
+$(function() {
+      //スムーズスクロール
+      let runScroll = function() {
+          $('a[href^="#"]').click(function() {
+              let speed = 400;
+              let href = $(this).attr("href");
+              let $target = $(href == "#" || href == "" ? 'html' : href);
+              let position = $target.offset().top - 100;
+
+              $('body,html').animate({
+                  scrollTop: position
+              }, speed, 'swing');
+              return false;
+          });
+      };
+
+      // ハンバーガーメニューチェックボックス外す処理
+      let unlockCheckBox = function() {
+          let $trigger = $('.header__navi a');
+          let $target = $('input[type="checkbox"]');
+
+          $trigger.click(function() {
+              $target.prop('checked', false);
+          });
+      };
+
+      // Document Ready
+          runScroll();
+          unlockCheckBox();
+      });
